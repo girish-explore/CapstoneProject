315469abbffb9cb1f055c5616c7e09e4cc54caf7
First Trail <gbabaiahgari@gmail.com>
2024-11-05 10:11:55 +0530

    Changes in resgistration module

--- registration/src/main/java/com/sbi/registration/repository/SystemDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sbi.registration.repository;
-
-import com.sbi.registration.entity.SystemData;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SystemDataRepository extends JpaRepository<SystemData, String> {
-}
--- registration/src/main/java/com/sbi/registration/service/SystemDataService.java
+++
@@ -1,7 +0,0 @@
-package com.sbi.registration.service;
-
-import com.sbi.registration.entity.SystemData;
-
-public interface SystemDataService {
-    SystemData newCustomer(SystemData newCustomer);
-}
